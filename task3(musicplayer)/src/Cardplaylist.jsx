3b0b94a28aeccaad9481d0af26e329c7d2d6632f
yogeshjames <150313150+yogeshjames@users.noreply.github.com>
2024-07-11 03:30:50 +0530

    Update Cardplaylist.jsx

--- task3(musicplayer)/src/Cardplaylist.jsx
+++ task3(musicplayer)/src/Cardplaylist.jsx
@@ -6,7 +6,7 @@ function Cardplaylist({mop ,func ,u}){
     return(
         <>
          <div className='my-3 ml-3 bg-black shadow-md cursor-pointer rounded-md w-60' onClick={()=>{func();
-            u(mop.id);
+            u(mop.songs);
          }}>
             <img src={mop.songs.length>0 ?mop.songs[0].album.images[0].url:""  } alt='helo' className="w-full h-40 object-cover rounded-md"></img>
             <div className=' flex justify-between items-center'>
