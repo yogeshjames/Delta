f5324d805de6d0d92911eb71569f5903f560cc23
yogeshjames <150313150+yogeshjames@users.noreply.github.com>
2024-08-02 00:26:28 +0530

    Update java.js

--- TASK1 NORMAL MODE/java.js
+++ TASK1 NORMAL MODE/java.js
@@ -930,14 +930,19 @@ function resumeTimers() {
 }
 
 // 
-function play() {
-    Paused = !Paused;
-    if (Paused) {
-        teampause = team; // S
-    } else {
-        team = teampause; // 
-        resumeTimers();
-    }
+function pauseGame() {
+  if (!Paused) {
+      Paused = true;
+      teampause = team; // Store the current team
+  }
+}
+
+function resumeGame() {
+  if (Paused) {
+      Paused = false;
+      team = teampause; // Restore the paused team state
+      resumeTimers();
+  }
 }
 
 
